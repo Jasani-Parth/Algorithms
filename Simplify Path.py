1db52f596e8f4a8999c1e85a52a0111fc702341b
Parth Jasani <82431676+Jasani-Parth@users.noreply.github.com>
2022-03-14 18:14:40 +0530

    leetcode

---
+++ Simplify Path.py
@@ -0,0 +1,25 @@
+'''
+- https://leetcode.com/problems/simplify-path/
+'''
+
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        
+        DIR = path.split('/')[1:]
+        L = list()
+        
+        for directory in DIR :
+            
+            if directory == "." or directory == "":
+                continue
+                
+            elif directory == ".." :
+                
+                if len(L) != 0 :
+                    L.pop()
+            
+            else :
+                
+                L.append(directory)
+        
+        return "/" + "/".join(L)
